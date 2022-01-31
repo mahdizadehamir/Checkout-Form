5d371c60d83326606b05f698e1693d441023c2a9
Amir <amir@Amirs-MacBook-Air.local>
2022-01-31 18:13:49 +0330

    delete button work correctly in basketPage

--- src/store/modules/basket.js
+++ src/store/modules/basket.js
@@ -3,16 +3,18 @@ export default {
     state: () => ({
         selectedItems: [],
         basketItems: [],
-        counter:{}
+        counter: {},
     }),
-
+    // getters is change the value of States
     getters: {
-       
-       deleteItem:(state) => (payload) => {
-           state.basketItems =  state.basketItems.filter(((v,i,a) => { return v.id != payload.id}))
-       }
-      
-        
+        deleteItem: (state) => (payload) => {
+            // first find my elemet Id and Then delete it from Basket Items
+            const findElementId = (element) => {
+                return element.id === payload.id
+            }
+            const elementId = state.basketItems.findIndex(findElementId)
+            state.basketItems.splice(elementId, 1)
+        },
     },
 
     mutations: {
@@ -31,20 +33,26 @@ export default {
             }
             //copying counts object to BasketItems
             for (let product in counts) {
-                state.basketItems.push({ "id": parseInt(product), "qun": counts[product] })
+                state.basketItems.push({
+                    id: parseInt(product),
+                    qun: counts[product],
+                })
                 // if(state.basketItems.length > 4){
                 // state.basketItems.shift()}
-
             }
-            state.basketItems = state.basketItems.slice().reverse().filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i).reverse()
+            state.basketItems = state.basketItems
+                .slice()
+                .reverse()
+                .filter((v, i, a) => a.findIndex((t) => t.id === v.id) === i)
+                .reverse()
             console.log(state.basketItems)
         },
-        updateCounter(state,payload){
+        updateCounter(state, payload) {
             // const findItems = state.basketItems.findIndex(x => (x.id === payload.id))
             // state.basketItems[findItems].qun = payload.qun
             state.counter = payload
             console.log(state.payload)
-        }
+        },
     },
 
     actions: {
