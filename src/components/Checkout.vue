4f89c3b633bedbc0bfce1df7cc0a64031d8dc80f
Amir <amir@Amirs-MacBook-Air.local>
2022-02-10 06:57:17 +0330

    end of the day

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="font-bold text-center mt-5">فروشگاه اینترنتی امیران</div>
-    <div
-        dir="rtl"
-        class="flex flex-row p-4 justify-center  mt-4 w-full "
-    >
+    <div dir="rtl" class="flex flex-row p-4 justify-center mt-4 w-full">
         <router-link
             class="rounded p-2 border-2 border-black"
             :to="{ name: 'Basket' }"
@@ -21,7 +18,7 @@
             "
         ></div>
         <router-link
-            class="rounded p-2 border-2  bg-red-500 text-white "
+            class="rounded p-2 border-2 bg-red-500 text-white"
             :to="{ name: 'Checkout' }"
             >دریافت آدرس پستی و نحوه ارسال</router-link
         >
@@ -42,10 +39,68 @@
             >نحوه پرداخت</router-link
         >
     </div>
+    <div class="flex flex-row border-2 border-black mt-2 p-2 md:w-5/6 mx-auto">
+        <div class="sammary w-1/4 border-2 border-black"></div>
+        <div dir="rtl" class="order w-3/4 border-2 border-black">
+            <h2 class="font-bold p-1">اطلاعات شناسایی و آدرس پستی</h2>
+            <form class="mt-2 p-3" action="">
+                <label class="block" for="firstname"> نام:</label>
+                <input
+                    class="mt-1 border-2 border-black w-40"
+                    name="firstname"
+                    type="text"
+                />
+                <label class="block mt-2" for="lastname">نام خانوادگی :</label>
+                <input
+                    type="text"
+                    name="lastname"
+                    class="mt-1 border-2 border-black w-40"
+                />
+                <label class="block mt-2" for="address"
+                    >آدرس تحویل مرسوله :</label
+                >
+                <input
+                    class="border-2 border-black"
+                    type="text"
+                    name="address"
+                />
+                <input
+                    class="border-2 border-black block mt-2"
+                    type="text"
+                    placeholder="ادامه آدرس"
+                />
+                <label class="block mt-2" for="state">استان :</label>
+                <select v-model="myProvince" class="mt-1" name="state" >
+                    <option  v-for="province in provinces" :key="province" :value="province.id">
+                        {{province.name}}
+                    </option>
+                </select>
+                <label class="block mt-2 " for="city">شهر :</label>
+                <select  class="mt-1" name="city " >
+                    <option >
+                        {{findOb(myProvince)}}
+                    </option>
+                </select>
+            </form>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {}
+import { mapGetters, mapState } from 'vuex'
+export default {
+    data(){
+        return{
+        myProvince:null}
+    },
+    computed:{
+        ...mapState('provinces',['provinces'])
+    },
+    methods:{
+        ...mapGetters('provinces',['findOb'])
+    }
+    
+}
 </script>
 
 <style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import { createStore } from 'vuex'
 import createPersistedState from "vuex-persistedstate";
 import products from '../store/modules/products'
 import basket from '../store/modules/basket'
-
+import provinces from '../store/modules/provinces'
 const store = createStore({
     modules: {
-        products,basket
+        products,basket,provinces
     },
     plugins : [createPersistedState()]
 })
